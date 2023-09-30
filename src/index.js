ffc29245ba8b24bc187e2de612e5757e45173c9a
Divyaa Sharmaa <shdivya1310@gmail.com>
2023-09-30 12:45:10 +0530

    Greeting Message

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let curDate = new Date(2023, 5, 6, 11);
+curDate = curDate.getHours();
+let greeting ='';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const hel={
+  background: 'aqua'
+};
+
+const greet = {
+  color: 'blue',
+  textAlign: 'center',
+  fontWeight: 'bold',
+  background: 'lightPink'
+};
+
+if(curDate>=1 && curDate<12){
+  greeting = 'Good Morning';
+  greet.color = 'grey';
+}else if(curDate>=12 && curDate<16){
+   greeting = 'Good Afternoon';
+   greet.color = 'red';
+}else if(curDate>=16 && curDate<19){
+  greeting = 'Good Evening';
+  greet.color = 'purple';
+}
+else if(curDate>=19 && curDate<24){
+  greeting = 'Good Night'
+  greet.color = 'black';
+}
+
+ReactDOM.render(
+       <h1 style={hel}>Hello Sir, <span style={greet}>{greeting}</span></h1>
+     ,document.getElementById('root')
+     );
+
